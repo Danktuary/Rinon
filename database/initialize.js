db582fc7ce2979d0e2051098d18e0a1c959671fc
Sanctuary <sanc.pw@gmail.com>
2017-11-05 02:28:19 -0300

    Remove database files, update package.json
    No database needed; found a workaround

--- database/initialize.js
+++
@@ -1,7 +0,0 @@
-const sequelize = require('./connection');
-
-sequelize.import('models/Poll');
-
-sequelize.sync()
-	.then(() => console.log('Database loaded'))
-	.catch(console.error);
--- database/modelObjects.js
+++
@@ -1,5 +0,0 @@
-const sequelize = require('./connection');
-
-const Poll = sequelize.import('models/Poll');
-
-module.exports = { Poll };
