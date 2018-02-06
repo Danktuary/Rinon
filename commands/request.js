c6d58f8a44ff4f35d87b16f0cdca0136119819e4
Sanctuary <sanc.pw@gmail.com>
2018-02-05 21:30:29 -0300

    Add duplicate emoji name checking feature
    Closes #1

--- commands/request.js
+++ commands/request.js
@@ -17,30 +17,55 @@ const request = {
 			return message.reply([
 				'you need to provide an emoji, a name and an emoji, a name and an image URL, or a name and an image file!',
 				`For example: \`${prefix}request rinon https://i.imgur.com/7QeCxca.jpg\`.`,
-			]);
+			].join('\n'));
 		}
 
 		try {
 			args = RequestParser.parse(message, args);
-		}
-		catch (error) {
-			return message.reply(error.message);
-		}
-
-		try {
 			GuildManager.checkEmojiAmount(message.guild, args[1]);
 		}
 		catch (error) {
 			return message.reply(error.message);
 		}
 
-		const response = await snekfetch.get(args[1]).catch(error => error);
+		const imageData = await snekfetch.get(args[1]).catch(error => error);
 
-		if (!response.ok) {
+		if (!imageData.ok) {
 			return message.reply('that image link doesn\'t seem to be working.');
 		}
-		else if (response.headers['content-length'] > (256 * 1000)) {
+		else if (imageData.headers['content-length'] > (256 * 1000)) {
 			return message.reply('that file surpasses the 256kb file size limit! Please resize it and try again.');
+		}
+
+		const duplicatedEmojis = message.client.emojis.filter(emoji => {
+			return args[0].toLowerCase() === emoji.name.toLowerCase();
+		});
+
+		if (duplicatedEmojis.size) {
+			try {
+				await message.reply([
+					'It seems like I have other emojis with that same name!\n',
+					'Does your request match any of the following emojis: ',
+					duplicatedEmojis.map(emoji => emoji.toString()).join(' '),
+					'\nIf so, would you like to cancel your request?',
+				].join(''));
+
+				const filter = m => ['yes', 'y', 'no', 'n'].includes(m.content.toLowerCase());
+				const options = { max: 1, time: 15000, errors: ['time'] };
+
+				const response = await message.channel.awaitMessages(filter, options)
+					.then(responses => responses.first().content);
+
+				if (['yes', 'y'].includes(response.toLowerCase())) {
+					return message.channel.send('Got it; I\'ve cancelled your request.');
+				}
+				else {
+					await message.channel.send('Got it; I\'ll continue on with your original request.');
+				}
+			}
+			catch (error) {
+				await message.reply('you didn\'t reply in time; I\'ll continue on with your original request.');
+			}
 		}
 
 		return Poll.create(message, args);
