e8c321f5e5d04b0ef76759c8207b76400c60e62a
Sanctuary <sanc.pw@gmail.com>
2018-03-11 21:32:23 -0300

    Add Poll model

---
+++ db/models/Poll.js
@@ -0,0 +1,16 @@
+const Poll = (sequelize, DataTypes) => {
+	return sequelize.define('Poll', {
+		message_id: DataTypes.STRING,
+		author_id: DataTypes.STRING,
+		emoji_name: DataTypes.STRING,
+		image_url: DataTypes.STRING,
+		approved: {
+			type: DataTypes.BOOLEAN,
+			defaultValue: false,
+		},
+	}, {
+		underscored: true,
+	});
+};
+
+module.exports = Poll;
