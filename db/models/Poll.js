b649eb32aa97a5c37f5511b188f3807ba43f40ce
Sanctuary <sanc.pw@gmail.com>
2018-03-12 01:04:58 -0300

    Disallow most Poll model fields from being null, add emoji_id column

--- db/models/Poll.js
+++ db/models/Poll.js
@@ -2,14 +2,29 @@ const Poll = (database, DataTypes) => {
 	return database.define('Poll', {
 		message_id: {
 			type: DataTypes.STRING,
+			allowNull: false,
 			unique: true,
 		},
-		author_id: DataTypes.STRING,
-		emoji_name: DataTypes.STRING,
-		image_url: DataTypes.STRING,
+		author_id: {
+			type: DataTypes.STRING,
+			allowNull: false,
+		},
+		emoji_name: {
+			type: DataTypes.STRING,
+			allowNull: false,
+		},
+		image_url: {
+			type: DataTypes.STRING,
+			allowNull: false,
+		},
 		status: {
 			type: DataTypes.ENUM('pending', 'approved', 'denied'),
 			defaultValue: 'pending',
+			allowNull: false,
+		},
+		emoji_id: {
+			type: DataTypes.STRING,
+			unique: true,
 		},
 	}, {
 		underscored: true,
