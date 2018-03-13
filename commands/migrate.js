940b50fdc3d5ce40b56d0692771548661a7cbfa0
Sanctuary <sanc.pw@gmail.com>
2018-03-13 01:57:15 -0300

    Update migration script

--- commands/migrate.js
+++ commands/migrate.js
@@ -1,4 +1,4 @@
-const { Poll } = require('../db/models/');
+const { Emoji, Poll } = require('../db/models/');
 
 const { colors } = require('../config');
 
@@ -9,16 +9,17 @@ const usernameUpdates = { synicalsyntax: 'ikemendia' };
 const migrate = {
 	name: 'migrate',
 	description: 'Take the existing, approved/denied polls and store them in a database, for statistics and such.',
-	async execute(message, args) {
-		const [after, limit] = args;
+	ownerOnly: true,
+	async execute(message, [limit = 100, after]) {
 		const channel = message.guild.channels.find('name', 'emoji-voting');
 
 		const messages = await channel.messages.fetch({ after, limit });
-		const approvedMessages = messages.filter(m => m.embeds.length && m.embeds[0].color);
+		const approvedMessages = messages
+			.sort((a, b) => a.createdTimestamp - b.createdTimestamp)
+			.filter(m => m.embeds.length && m.embeds[0].color);
 
 		for (const m of approvedMessages.values()) {
 			const [embed] = m.embeds;
-
 			let [, username] = embed.author.name.match(/Request by (\w+)#\d{4}/);
 
 			if (usernameUpdates.hasOwnProperty(username)) {
@@ -35,14 +36,16 @@ const migrate = {
 				updated_at: m.editedAt,
 			};
 
+			let emoji;
+
 			if (embed.color === colors.approved) {
 				const [, animated, emojiName, emojiID] = embed.description.match(emojiRegex);
 
-				// TODO: emojiID is no longer on the Poll model; need to make a new row in the Emoji model
+				emoji = await Emoji.create({ emojiID, guildID: message.guild.id });
+
 				data = Object.assign(data, {
 					emojiName,
 					imageURL: `${baseEmojiURL}/${emojiID}.${animated ? 'gif' : 'png'}`,
-					emojiID,
 				});
 			}
 			else {
@@ -52,11 +55,14 @@ const migrate = {
 				});
 			}
 
-			console.log(data);
-			console.log();
+			console.log(data, '\n');
 
-			await Poll.create(data, { silent: true });
+			const poll = await Poll.create(data, { silent: true });
+
+			if (emoji) poll.setEmoji(emoji);
 		}
+
+		return message.channel.send('Done!');
 	},
 };
 
