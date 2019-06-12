fec4a8ab7cd521967c5eb568d31fab78aa924071
Sanctuary <sanc.pw@gmail.com>
2019-06-12 09:42:42 -0400

    Add confirmation prompt to init command

--- commands/init.js
+++ commands/init.js
@@ -44,6 +44,28 @@ module.exports = class InitCommand extends Command {
 		return message;
 	}
 
+	async confirmInit(message) {
+		await message.channel.send([
+			'This will delete all other channels in this server and setup the necessary ones.',
+			'I\'ll also try to configure the guild settings appropriately, if possible. Continue?',
+		]);
+
+		let response;
+		const options = { max: 1, time: 20000, errors: ['time'] };
+		const filter = m => ['yes', 'y', 'no', 'n'].includes(m.content.toLowerCase());
+
+		try {
+			const responses = await message.channel.awaitMessages(filter, options);
+			response = responses.first().content.toLowerCase();
+		} catch (error) {
+			throw new Error('You didn\'t reply in time. Initialization cancelled.');
+		}
+
+		if (response && ['no', 'n'].includes(response)) {
+			throw new Error('Got it, I\'ve cancelled the initialization.');
+		}
+	}
+
 	async setupChannels(guild) {
 		if (guild.channels.some(channel => ['emoji-voting', 'rinon-testing'].includes(channel.name))) return;
 
@@ -71,6 +93,12 @@ module.exports = class InitCommand extends Command {
 
 		if (guild.channels.some(channel => channel.name === 'emoji-voting')) {
 			return message.channel.send('I\'m already good to go!');
+		}
+
+		try {
+			await this.confirmInit(message);
+		} catch (error) {
+			return message.channel.send(error.message);
 		}
 
 		const channels = guild.channels.filter(channel => channel.id !== message.channel.id);
