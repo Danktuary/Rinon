6baca30b64e97a86221a84ab07487974813181d5
Sanctuary <sanc.pw@gmail.com>
2019-06-12 21:30:01 -0400

    Add basic about command

---
+++ commands/about.js
@@ -0,0 +1,33 @@
+const { RichEmbed } = require('discord.js');
+const { Command } = require('discord-akairo');
+const { colors } = require('../config.js');
+const permissionsUtil = require('../util/permissions.js');
+const emojiUtil = require('../util/emoji.js');
+
+module.exports = class AboutCommand extends Command {
+	constructor() {
+		super('about', {
+			aliases: ['about'],
+		});
+	}
+
+	async exec(message) {
+		const { client } = message;
+		const owner = await client.fetchUser(client.ownerID);
+		const invite = await client.generateInvite(permissionsUtil.required);
+		const emojis = { total: client.emojis.size, ...emojiUtil.getAmounts(client.emojis) };
+
+		const embed = new RichEmbed()
+			.setColor(colors.misc)
+			.setThumbnail(client.user.displayAvatarURL)
+			.setFooter(`Author: ${owner.tag}`, owner.displayAvatarURL)
+			.setDescription([
+				`Use \`${this.handler.prefix()}help\` to get a list of all my commands!`,
+				`[Invite](${invite}) | [GitHub](https://github.com/Danktuary/Rinon)`,
+			].join('\n'))
+			.addField('Servers', `${client.guilds.size} (${client.hubServer.serverList})`, true)
+			.addField('Emojis', `${emojis.total} (${emojis.animated} animated, ${emojis.normal} normal)`, true)
+
+		return message.channel.send(embed);
+	}
+};
