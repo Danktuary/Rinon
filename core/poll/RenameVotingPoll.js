dee81b19a1bd355dc200ac319d6d5723d7413a22
Sanctuary <sanc.pw@gmail.com>
2019-06-21 01:09:09 -0400

    Add approve and deny methods to RenameVotingPoll class

--- core/poll/RenameVotingPoll.js
+++ core/poll/RenameVotingPoll.js
@@ -1,5 +1,6 @@
 const Poll = require('./index.js');
 const emojiUtil = require('../../util/emoji.js');
+const regexes = require('../../util/regexes.js');
 const models = require('../../database/models/index.js');
 
 module.exports = class RenameVotingPoll extends Poll {
@@ -9,12 +10,56 @@ module.exports = class RenameVotingPoll extends Poll {
 	}
 
 	async create({ message, oldName, newName }) {
-		const emoji = emojiUtil.search(this.client.emojis, oldName);
+		// TODO: Account for multiple results
+		const emoji = emojiUtil.search(this.client.emojis, oldName).first();
+		return this.sendEmbed({
+			thumbnail: emoji.url,
+			author: message.author,
+			description: `\`${message.author.tag}\` wants to rename ${emoji} from \`${emoji.name}\` to \`${newName}\`.`,
+		});
+	}
+
+	async approve(message) {
+		const { oldName, newName, author, emoji } = await this._parseData(message);
+		const emojiData = await models.Emoji.findOne({ where: { emojiID: emoji.id }, include: ['poll'] });
+
+		emojiData.poll.emojiName = newName;
+		await message.delete();
+		await emoji.edit({ name: newName });
+		await emojiData.save();
 
 		return this.sendEmbed({
-			emoji,
-			author: message.author,
-			description: `\`${message.author.tag}\` wants to rename ${emoji} from \`${oldName}\` to \`${newName}\`.`,
+			author,
+			status: 'approved',
+			thumbnail: emoji.url,
+			channel: this.client.hubServer.approvedRenames,
+			description: `\`${oldName}\` has been renamed to \`${newName}\`! ${emoji}`,
 		});
+	}
+
+	async deny(message, reason) {
+		const { oldName, newName, author, emoji } = await this._parseData(message);
+		await message.delete();
+		return this.sendEmbed({
+			author,
+			status: 'denied',
+			thumbnail: emoji.url,
+			channel: this.client.hubServer.deniedRenames,
+			description: reason || `Renaming \`${oldName}\` to \`${newName}\` has been denied. :(`,
+		});
+	}
+
+	async _parseData(message) {
+		const { client, embeds: [embed] } = message;
+		const [, authorID] = embed.author.name.match(/\((\d+)\)/);
+		const [, oldName, newName] = embed.description.match(/from `(\w+)` to `(\w+)`\./);
+		const emojiID = embed.description.match(regexes.emoji)[3];
+
+		return {
+			oldName,
+			newName,
+			author: await client.fetchUser(authorID),
+			emoji: client.emojis.get(emojiID),
+		};
 	}
 };
