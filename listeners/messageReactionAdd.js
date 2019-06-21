7c98bf76d1a3e1c822c7b428a8545b14ee95175c
Sanctuary <sanc.pw@gmail.com>
2019-06-20 22:03:07 -0400

    Refactor messageReactionAdd event

--- listeners/messageReactionAdd.js
+++ listeners/messageReactionAdd.js
@@ -1,5 +1,4 @@
 const { Listener } = require('discord-akairo');
-const poll = require('../core/poll.js');
 const models = require('../database/models/');
 const { emojis, voteEndAmount } = require('../config');
 
@@ -9,16 +8,16 @@ module.exports = class MessageReactionAddListener extends Listener {
 	}
 
 	async exec(reaction, user) {
-		if (user.bot || reaction.message.channel.name !== 'emoji-voting') return;
-		if (![emojis.approve, emojis.deny].includes(reaction.emoji.id)) return;
-
 		const { message } = reaction;
-		const [embed] = message.embeds;
+		const { client, embeds: [embed] } = message;
+		const poll = Object.values(client.hubServer.polls).find(p => p.channel.id === message.channel.id);
 
+		if (user.bot || !poll) return;
+		if (![emojis.approve, emojis.deny].includes(reaction.emoji.id)) return;
 		if (!embed || embed.color) return;
 
 		const pollData = await models.Poll.findOne({ where: { messageID: message.id } });
-		const endAmount = (reaction.users.has(pollData.authorID)) ? voteEndAmount + 2 : voteEndAmount + 1;
+		const endAmount = reaction.users.has(pollData.authorID) ? voteEndAmount + 2 : voteEndAmount + 1;
 
 		if (reaction.count < endAmount) return;
 		if (reaction.emoji.id === emojis.deny) return poll.deny(message);
