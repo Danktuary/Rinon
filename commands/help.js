eed29c25e14d836dff30342a6e41390b76074151
Sanctuary <sanc.pw@gmail.com>
2019-07-28 12:30:44 -0400

    Add basic help command

---
+++ commands/help.js
@@ -0,0 +1,42 @@
+const { Command } = require('discord-akairo');
+
+module.exports = class HelpCommand extends Command {
+	constructor() {
+		super('help', {
+			aliases: ['help'],
+			args: [
+				{
+					id: 'helpCommand',
+					match: 'word',
+				},
+			],
+		});
+	}
+
+	async exec(message, { helpCommand }) {
+		const data = [];
+		const commands = this.handler.modules;
+		const prefix = this.handler.prefix();
+
+		if (!helpCommand) {
+			data.push('Here\'s a list of all my commands:');
+			data.push(commands.map(command => command.aliases[0]).join(', '));
+			data.push(`\nYou can send \`${prefix}help [command name]\` to get info on a specific command!`);
+		} else {
+			if (!commands.has(helpCommand)) {
+				return message.reply(`that's not a valid command! Send \`${prefix}help\` to get a list of all commands.`);
+			}
+
+			const { aliases, description, options: { usage } } = commands.find(command => {
+				return command.aliases.includes(helpCommand);
+			});
+
+			data.push(`**Name:** ${aliases[0]}`);
+			if (aliases.length > 1) data.push(`**Aliases:** ${aliases.slice(1).join(', ')}`);
+			if (description) data.push(`**Description:** ${description}`);
+			if (usage) data.push(`**Usage:** \`${prefix}${aliases[0]} ${usage}\``);
+		}
+
+		return message.channel.send(data, { split: true });
+	}
+};
