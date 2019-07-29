ac48b9520002313c0632667bb48501d542c6a1b5
Sanctuary <sanc.pw@gmail.com>
2019-07-28 20:01:40 -0400

    Rename/lint the init command

--- commands/initialize.js
+++ commands/initialize.js
@@ -2,10 +2,10 @@ const { Command } = require('discord-akairo');
 const { MessageEmbed } = require('discord.js');
 const permissionsUtil = require('../util/permissions.js');
 
-module.exports = class InitCommand extends Command {
+module.exports = class InitializeCommand extends Command {
 	constructor() {
-		super('init', {
-			aliases: ['init'],
+		super('initialize', {
+			aliases: ['initialize', 'init'],
 			userPermissions: ['MANAGE_GUILD'],
 			args: [
 				{
@@ -16,6 +16,60 @@ module.exports = class InitCommand extends Command {
 			],
 		});
 	}
+
+	async exec(message, { skipConfirmation }) {
+		const { guild } = message.channel;
+		const missingPerms = this.missingPermissions(guild.me);
+
+		if (missingPerms.length) {
+			return message.channel.send(this.formatErrors([{
+				title: 'Missing Permissions',
+				body: `I'm missing the following permissions: ${missingPerms}`,
+			}]));
+		}
+
+		if (guild.channels.some(channel => channel.name === 'emoji-voting')) {
+			return message.channel.send('I\'m already good to go!');
+		}
+
+		if (!skipConfirmation) {
+			try {
+				await this.confirmInit(message);
+			} catch (error) {
+				return message.channel.send(error.message);
+			}
+		}
+
+		const channels = guild.channels.filter(channel => channel.id !== message.channel.id);
+
+		try {
+			await Promise.all(channels.map(channel => channel.delete()));
+			await this.setupChannels(guild);
+		} catch (error) {
+			console.error('Failed to initialize channels.\n', error);
+			return message.channel.send('There was an error setting up the channels.');
+		}
+
+		if (guild.me.hasPermission('MANAGE_GUILD')) {
+			try {
+				await guild.setSystemChannel(null);
+				await guild.setDefaultMessageNotifications('MENTIONS');
+			} catch (error) {
+				await message.channel.send([
+					'Error trying to adjust the server\'s current settings.',
+					'Server default message notifications and system channel settings left unconfigured.',
+				]);
+			}
+		} else {
+			await message.channel.send([
+				'There are some extra settings I can adjust for you if I\'m given the "Manage Server" permission.',
+				'Server default message notifications and system channel left unconfigured.',
+			]);
+		}
+
+		return message.channel.send(`Done! You can now use the \`${this.handler.prefix()}add\` command to create polls.`);
+	}
+
 
 	missingPermissions(clientMember, format = true) {
 		const missingPerms = clientMember.permissions.missing(permissionsUtil.required);
@@ -73,58 +127,5 @@ module.exports = class InitCommand extends Command {
 			{ id: guild.defaultRole.id, denied: ['VIEW_CHANNEL'] },
 			{ id: guild.me.id, allowed: ['VIEW_CHANNEL'] },
 		]);
-	}
-
-	async exec(message, { skipConfirmation }) {
-		const { guild } = message.channel;
-		const missingPerms = this.missingPermissions(guild.me);
-
-		if (missingPerms.length) {
-			return message.channel.send(this.formatErrors([{
-				title: 'Missing Permissions',
-				body: `I'm missing the following permissions: ${missingPerms}`,
-			}]));
-		}
-
-		if (guild.channels.some(channel => channel.name === 'emoji-voting')) {
-			return message.channel.send('I\'m already good to go!');
-		}
-
-		if (!skipConfirmation) {
-			try {
-				await this.confirmInit(message);
-			} catch (error) {
-				return message.channel.send(error.message);
-			}
-		}
-
-		const channels = guild.channels.filter(channel => channel.id !== message.channel.id);
-
-		try {
-			await Promise.all(channels.map(channel => channel.delete()));
-			await this.setupChannels(guild);
-		} catch (error) {
-			console.error('Failed to initialize channels.\n', error);
-			return message.channel.send('There was an error setting up the channels.');
-		}
-
-		if (guild.me.hasPermission('MANAGE_GUILD')) {
-			try {
-				await guild.setSystemChannel(null);
-				await guild.setDefaultMessageNotifications('MENTIONS');
-			} catch (error) {
-				await message.channel.send([
-					'Error trying to adjust the server\'s current settings.',
-					'Server default message notifications and system channel settings left unconfigured.',
-				]);
-			}
-		} else {
-			await message.channel.send([
-				'There are some extra settings I can adjust for you if I\'m given the "Manage Server" permission.',
-				'Server default message notifications and system channel left unconfigured.',
-			]);
-		}
-
-		return message.channel.send(`Done! You can now use the \`${this.handler.prefix()}add\` command to create polls.`);
 	}
 };
