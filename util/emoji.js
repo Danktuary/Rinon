6c3548d31266e2d1a0c75ebd380e9bfeb45119d0
Sanctuary <sanc.pw@gmail.com>
2019-11-21 13:45:55 -0500

    Remove checkAmounts function

--- util/emoji.js
+++ util/emoji.js
@@ -1,4 +1,3 @@
-const { prefix } = require('../config.js');
 const parseInput = require('../util/parseInput.js');
 const regexes = require('../util/regexes.js');
 
@@ -7,23 +6,6 @@ const boostedEmojisLimits = { 0: 50, 1: 100, 2: 150, 3: 250 };
 function getAmounts(emojis) {
 	const [normal, animated] = emojis.partition(emoji => !emoji.animated);
 	return { normal: normal.size, animated: animated.size };
-}
-
-function checkAmounts(emojis, emojiURL) {
-	const { normal, animated } = getAmounts(emojis);
-
-	const maxEmojisReply = [
-		'It seems like I can\'t add any more emojis to this server.',
-		`Want to check the other servers I'm in? Use the \`${prefix}server\` command!`,
-	].join('\n');
-
-	if (normal + animated === 100) {
-		throw new Error(maxEmojisReply);
-	} else if (emojiURL.endsWith('.gif') && animated === 50) {
-		throw new Error(maxEmojisReply.replace('emojis', 'animated emojis'));
-	} else if (emojiURL.endsWith('.png') && normal === 50) {
-		throw new Error(maxEmojisReply.replace('emojis', 'static emojis'));
-	}
 }
 
 function search(emojis, searchTerm) {
