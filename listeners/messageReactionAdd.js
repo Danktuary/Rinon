0fb438978052a438b9c096be7def992d6ed3d280
Sanctuary <sanc.pw@gmail.com>
2019-12-01 20:40:56 -0500

    Refact message reaction listener

--- listeners/messageReactionAdd.js
+++ listeners/messageReactionAdd.js
@@ -7,19 +7,27 @@ module.exports = class MessageReactionAddListener extends Listener {
 	}
 
 	async exec(reaction, user) {
+		if (user.bot) return;
+
 		const { message } = reaction;
-		const { client, embeds: [embed] } = message;
-		const poll = Object.values(client.hubServer.polls).find(p => p.channel.id === message.channel.id);
+		const { client } = message;
 
-		if (user.bot || !poll) return;
+		if (message.channel.id !== client.hubServer.votingChannel.id) return;
 		if (![emojis.approve, emojis.deny].includes(reaction.emoji.id)) return;
-		if (!embed || embed.color) return;
 
+		const poll = this._getPoll(message);
 		const pollData = await poll.model.findOne({ where: { messageID: message.id } });
 		const endAmount = reaction.users.has(pollData.authorID) ? voteEndAmount + 2 : voteEndAmount + 1;
 
 		if (reaction.count < endAmount) return;
 		if (reaction.emoji.id === emojis.deny) return poll.deny({ message });
 		return poll.approve({ message });
+	}
+
+	_getPoll(message) {
+		const { client, embeds: [embed] } = message;
+		if (embed.description.includes('wants to add')) return client.hubServer.polls.emoji;
+		if (embed.description.includes('wants to rename')) return client.hubServer.polls.rename;
+		return null;
 	}
 };
