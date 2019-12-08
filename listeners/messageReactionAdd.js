5cd6507d91ea18d1dcdeae6e9b18bcd8e7550d42
Sanctuary <sanc.pw@gmail.com>
2019-12-08 17:09:26 -0500

    client destructuring -> this.client

--- listeners/messageReactionAdd.js
+++ listeners/messageReactionAdd.js
@@ -10,9 +10,8 @@ module.exports = class MessageReactionAddListener extends Listener {
 		if (user.bot) return;
 
 		const { message } = reaction;
-		const { client } = message;
 
-		if (message.channel.id !== client.hubServer.votingChannel.id) return;
+		if (message.channel.id !== this.client.hubServer.votingChannel.id) return;
 		if (![emojis.approve, emojis.deny].includes(reaction.emoji.id)) return;
 
 		const poll = this._getPoll(message);
@@ -25,9 +24,9 @@ module.exports = class MessageReactionAddListener extends Listener {
 	}
 
 	_getPoll(message) {
-		const { client, embeds: [embed] } = message;
-		if (embed.description.includes('wants to add')) return client.hubServer.polls.emoji;
-		if (embed.description.includes('wants to rename')) return client.hubServer.polls.rename;
+		const { embeds: [embed] } = message;
+		if (embed.description.includes('wants to add')) return this.client.hubServer.polls.emoji;
+		if (embed.description.includes('wants to rename')) return this.client.hubServer.polls.rename;
 		return null;
 	}
 };
