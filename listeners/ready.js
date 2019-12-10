047796268b81d0d45b11e604cd3570ee252efbba
Sanctuary <sanc.pw@gmail.com>
2019-12-09 21:50:06 -0500

    Set client presence in ready event

--- listeners/ready.js
+++ listeners/ready.js
@@ -9,6 +9,8 @@ module.exports = class ReadyListener extends Listener {
 	}
 
 	exec() {
-		console.log(`${this.client.user.tag} ready!`);
+		const { client } = this;
+		console.log(`${client.user.tag} ready!`);
+		client.user.setPresence({ game: { name: `${client.emojis.size} emojis in ${client.guilds.size} servers` } });
 	}
 };
